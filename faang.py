731d1535446a0300b6e8cd8bfb60947d2c863248
Mariane McGrath <marianemcgrath@gmail.com>
2025-12-19 18:47:06 +0000

    Fix P2_P3tidy_P4Full

--- faang.py
+++ faang.py
@@ -1,37 +1,57 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-# Import yfinance and getting the data ready to run
-from fileinput import filename
+import pandas as pd
 import yfinance as yf
-
-# Import Dates and Times
+import os
 import datetime as dt
-
-# Import matplotlib for plotting the data
 import matplotlib.pyplot as plt
 
-# Download data for the required stocks (FAANG)
-df = yf.download(['META', 'AAPL','AMZN', 'NFLX', 'GOOG'], period='5d', interval='1h', auto_adjust=True)
+def get_data():
+    tickers = ["META", "AAPL", "AMZN", "NFLX", "GOOG"] 
+    df = yf.download(tickers, period='5d', interval='1h', auto_adjust=True) 
+    
+    if not os.path.exists('data'):      
+        os.makedirs('data')
+    
+    now = dt.datetime.now()
+    filename = f'data/{now.strftime("%Y%m%d-%H%M%S")}.csv'
+    df.to_csv(filename)
 
-# Save the data frame to CSV
-now= dt.datetime.now()
+    print(f"Data saved to {filename}")
+    
+    return df
 
-# Create the filename (and add formatting date and time) 
-filename_csv = 'data/' + now.strftime('%Y%m%d_%H%M%S') + ".csv"
+def plot_data():    
+    data_files = os.listdir('data')
+    data_files = [f for f in data_files if f.endswith('.csv')]
+    data_files.sort(reverse=True)
+    latest_file = data_files[0]
 
-# Save data to CSV File
-df.to_csv(filename_csv)
+    df = pd.read_csv(f'data/{latest_file}', header=[0, 1], index_col=0, parse_dates=True)
 
-# Plot the closing prices
-fig, ax = plt.subplots(figsize=(12, 6))
-df['Close'].plot(ax=ax)
-ax.set_title('FAANG Closing Prices')
-ax.set_xlabel('Date')
-ax.set_ylabel('Price')
-ax.legend(['META', 'AAPL', 'AMZN', 'NFLX', 'GOOG'])
+    if not os.path.exists('plots'):
+        os.makedirs('plots')
+    
+    closing_data = df['Close']
+    
+    now = dt.datetime.now()
+    fig, ax = plt.subplots(figsize=(12, 6))
+    closing_data.plot(ax=ax)
 
-# Filename with today's date
-filename_png = 'plots/' + now.strftime('%Y%m%d_%H%M%S') + '.png'
+    ax.set_title(f'FAANG Stock Closing Prices - {now.strftime("%Y-%m-%d")}')
+    ax.set_xlabel('Date and Time')
+    ax.set_ylabel('Closing Price (USD)')
+    ax.grid(True, alpha=0.3)
+    ax.legend(title='Stocks')
+    
+    plot_filename = f'plots/{now.strftime("%Y%m%d-%H%M%S")}.png'
+    fig.savefig(plot_filename, dpi=300, bbox_inches='tight')
+    plt.close(fig)
 
-# Save figure as an image file as today's date
-fig.savefig(filename_png, dpi=300)
+    print(f"Plot saved to {plot_filename}")
+    
+if __name__ == "__main__":
+    get_data()
+    plot_data()
+
+# Source: https://stackoverflow.com/questions/419163/what-does-if-name-main-do
